68ded751789c095378e6300c7f6be051a6e93f7e
takumi <free.swim.0723@icloud.com>
2021-03-10 14:08:21 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -25,3 +25,18 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/create', 'Admin\NewsController@add')->middleware('auth');
+});
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('profile/edit', 'Admin\NewsController@add')->middleware('auth');
+});
