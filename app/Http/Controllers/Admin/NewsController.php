face5d9f6dfca917f97f02474fbbbb8fb44c47fd
takumi <free.swim.0723@icloud.com>
2021-03-17 07:03:09 +0000

    課題提出

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\News;
+
 class NewsController extends Controller
 {
     //
@@ -15,6 +17,23 @@ class NewsController extends Controller
     
     public function create(Request $request)
     {
+        $this->validate($request, News::$rules);
+        
+        $news =new News;
+        $from = $request->all();
+        
+        if (isset($from['image'])) {
+            $path = $request->file('image')->store('public/image');
+            $news->image_path = basename($path);
+        } else {
+            $news->image_path = null;
+        }
+        unset($from['_token']);
+        unset($from['image']);
+        
+        $news->fill($from);
+        $news->save();
+        
         return resirect('admin/news/create');
     }
 }
