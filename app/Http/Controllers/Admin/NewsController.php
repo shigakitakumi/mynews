2a340f48b22de62934492b36f53bf582180ef755
takumi <free.swim.0723@icloud.com>
2021-03-21 08:45:11 +0000

    課題提出

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\Controller;
 
 use App\News;
 
+use App\History;
+
+use Carbon\Carbon;
+
 class NewsController extends Controller
 {
     //
@@ -20,20 +24,71 @@ class NewsController extends Controller
         $this->validate($request, News::$rules);
         
         $news =new News;
-        $from = $request->all();
+        $form = $request->all();
         
-        if (isset($from['image'])) {
+        if (isset($form['image'])) {
             $path = $request->file('image')->store('public/image');
             $news->image_path = basename($path);
         } else {
             $news->image_path = null;
         }
-        unset($from['_token']);
-        unset($from['image']);
+        unset($form['_token']);
+        unset($form['image']);
         
-        $news->fill($from);
+        $news->fill($form);
         $news->save();
         
-        return resirect('admin/news/create');
+        return redirect('admin/news/create');
     }
+    public function index(Request $request)
+    {
+        $cond_title = $request->cond_title;
+        if ($cond_title != '') {
+            $posts =News::where('title', $cond_title)->get();
+        } else {
+            $posts = News::all();
+        }
+        return view('admin.news.index', ['posts' => $posts, 'cond_title' => $cond_title]);
+    }
+    
+    public function edit(Request $request)
+    {
+        $news = News::find($request->id);
+        
+        return view('admin.news.edit', ['news_form' => $news]);
+    }
+    
+    public function update(Request $request)
+    {
+        $this->validate($request, News::$rules);
+        $news = News::find($request->id);
+        $news_form =$request->all();
+        if ($request->remove == 'true') {
+            $news_form['image_path'] = null;
+        } elseif ($request->file('image')) {
+            $path =$request->file('image')->store('public/image');
+            $news_form['image_path'] = basename($path);
+        } else {
+            $news_form['image_path'] = $news->image_path;
+        }
+        
+        unset($news_form['image']);
+        unset($news_form['remove']);
+        unset($news_form['_token']);
+        $news->fill($news_form)->save();
+        
+        $history = new History;
+        $history->news_id = $news->id;
+        $history->edited_at = Carbon::now();
+        $history->save();
+        
+        return redirect('admin/news');
+    }
+        
+    public function delete(Request $request)
+        {
+            $news = News::find($request->id);
+            $news->delete();
+            return redirect('admin/news/');
+        }
 }
--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
+use App\Profile;
+
 class ProfileController extends Controller
 {
     //
@@ -13,14 +15,26 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function create()
+    public function create(Request $request)
     {
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        unset($form['_token']);
+        $profile->fill($form);
+        $profile->save();
+        
         return redirect('admin/profile/create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+      $profile = Profile::find($request->id);
+
+        
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
     
     public function update()
