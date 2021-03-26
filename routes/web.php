52c328d8eecb259ecb04bef7d94e63e48d72ffc3
takumi <free.swim.0723@icloud.com>
2021-03-26 07:15:35 +0000

    課題提出

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,7 @@ Route::get('XXX', 'AAAController@bbb');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index');
+
+Route::get('/profile', 'ProfileController@index');
